286c52631340c98772597a262d998112fbeaf9be
Takeshi SHIMADA <tk.shimada.0912@gmail.com>
2017-06-09 00:22:44 +0900

    add letsencrypt

---
+++ ubuntu/letsencrypt.home/mkssl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+mkdir -p /etc/letsencrypt/ecdsa/archive
+cd /etc/letsencrypt/ecdsa/archive
+openssl ecparam -out privkey.pem -name secp384r1 -genkey
+cat << '__EOL__' > /tmp/lets_ecdsa
+[req]
+distinguished_name = dn
+[dn]
+[SAN]
+subjectAltName=DNS:ayurina.net, DNS:blog.ayurina.net, DNS:techblog.ayurina.net
+__EOL__
+openssl req -new -key privkey.pem -sha256 -nodes -outform der -out csr.der -subj "/CN=ayurina.net" -reqexts SAN -config /tmp/lets_ecdsa
+rm /tmp/lets_ecdsa
+certbot certonly \
+      --csr csr.der \
+      --no-self-upgrade \
+      -n \
+      --webroot \
+      --agree-tos \
+      --email tk.shimada.0912@gmail.com \
+      -w /var/www/html \
+      -d ayurina.net \
+      -w /var/www/html/wordpress \
+      -d blog.ayurina.net \
+      -w /var/www/html/techblog.ayurina.net \
+      -d techblog.ayurina.net
+
+mkdir -p /etc/letsencrypt/ecdsa/live
+ln -sf /etc/letsencrypt/ecdsa/archive/0000_cert.pem /etc/letsencrypt/ecdsa/live/cert.pem
+ln -sf /etc/letsencrypt/ecdsa/archive/0000_chain.pem /etc/letsencrypt/ecdsa/live/chain.pem
+ln -sf /etc/letsencrypt/ecdsa/archive/0001_chain.pem /etc/letsencrypt/ecdsa/live/fullchain.pem
+ln -sf /etc/letsencrypt/ecdsa/archive/privkey.pem /etc/letsencrypt/ecdsa/live/privkey.pem
